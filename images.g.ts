40eea9609d477aa727d7dc78ce76810d95761ee3
steveo <48230704+Darferrr@users.noreply.github.com>
2022-02-11 10:39:22 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . 2 2 . . . . 
+. . . . . . . . . 2 2 2 . . . . 
+. . . . . 2 2 2 2 2 2 2 . . . . 
+. . . . 2 2 2 2 2 2 2 2 2 . . . 
+. . . 2 2 f f f f f f 2 2 . . . 
+. . 2 2 2 2 f f f f f f 2 2 . . 
+. 2 2 2 2 2 2 2 c c c c 2 2 . . 
+f 2 2 2 2 2 2 2 c 1 f f 2 2 2 . 
+2 f 2 2 2 2 2 2 c 1 f f 5 5 5 5 
+f 2 2 2 2 2 2 2 2 b b b 5 5 5 5 
+2 f 2 2 2 2 2 2 2 2 2 2 2 2 5 5 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
